01050964bc3ad3a8f7ccb2ec0fa050b7cebbc062
Ezgi Keserci <keserci.ezgi@gmail.com>
2024-07-12 17:26:09 +0300

    Major overhaul: Refactor and EF Core integration
    - Refactored `IProductService` interface, removing `GetProducts` and adding `GetAll`, `GetAllByCategoryId`, and `GetByUnitPrice` for enhanced query functionality.
    - Updated `ProductManager` to align with `IProductService` changes, removing outdated logic and implementing new methods.
    - Transitioned data access layer to Entity Framework Core in `Program.cs`, including necessary package updates in `DataAccess.csproj`.
    - Adjusted project and solution files, including adding a new `Core` project for expanded architecture.
    - Introduced the generic repository pattern with `IEntityRepository` interface, implemented across product, category, and customer data access layers.
    - Configured `NorthwindContext` for Entity Framework Core, including entity class adjustments for EF operations.
    - Enhanced data access layer classes (`EfProductDal`, `EfCategoryDal`, `EfCustomerDal`) with comprehensive data manipulation methods using LINQ.
    - Prepared infrastructure for future expansions with `ICategoryDal` and `ICustomerDal` interfaces.
    
    This commit represents a significant architectural improvement, incorporating modern development practices for better scalability and maintainability.

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -10,6 +10,8 @@ namespace Business.Abstract
     // Service => Interface'ler için kullanılan terim.
     public interface IProductService
     {
-        List<Product> GetProducts();
+        List<Product> GetAll();
+        List<Product> GetAllByCategoryId(int categoryId);
+        List<Product> GetByUnitPrice(decimal min, decimal max);
     }
 }
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    // Generic constraint (where ....)
+    // class : Reference tip olmalı.
+    // IEntity: IEntity veya IEntity implement eden class olmalı. 
+    // new(): New'lenebilir olmalı.
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+        // Expression'lar sayesinde method içerisine LINQ sorguları gönderebiliriz.
+        // Func<input, result>
+        T Get(Expression<Func<T, bool>> filter);
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+    }
+}
