2fe7a7b7e04344cab7d0925178ffa1d5c224927e
Ezgi Keserci <keserci.ezgi@gmail.com>
2024-07-12 19:56:11 +0300

    Revert "test"
    This reverts commit 9213abd099d58eee208dd7f523b94b4e8b65d6d5.

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -9,6 +9,5 @@ namespace DataAccess.Abstract
     public interface IProductDal : IEntityRepository<Product>
     {
         List<ProductDetailDto> GetProductDetailDtos();
-        //deneme
     }
 }
