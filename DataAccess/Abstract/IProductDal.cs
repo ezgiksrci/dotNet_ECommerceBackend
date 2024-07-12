9213abd099d58eee208dd7f523b94b4e8b65d6d5
Ezgi Keserci <keserci.ezgi@gmail.com>
2024-07-12 19:55:13 +0300

    test

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -9,5 +9,6 @@ namespace DataAccess.Abstract
     public interface IProductDal : IEntityRepository<Product>
     {
         List<ProductDetailDto> GetProductDetailDtos();
+        //deneme
     }
 }
